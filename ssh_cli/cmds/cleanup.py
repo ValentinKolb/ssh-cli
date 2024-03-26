f8234c21578be5d2d3c8ffa62d3a44f890e4086e
valentinkolb <valentin.kolb@uni-ulm.de>
2024-03-26 16:31:37 +0100

    fix: won't delete still used files

--- ssh_cli/cmds/cleanup.py
+++ ssh_cli/cmds/cleanup.py
@@ -18,30 +18,33 @@ def _cleanup_key_files(c):
     used_key_files = [c.host(host).get("identityfile") for host in c.hosts()]
 
     # step 2, get all key files from key directory
-    key_files = set(
-        f.removesuffix(".pub")
+    found_key_files = set(
+        os.path.join(KEY_DIR_PATH, f).removesuffix(".pub")
         for f in os.listdir(KEY_DIR_PATH)
         if os.path.isfile(os.path.join(KEY_DIR_PATH, f))
     )
 
+    print(f"Used key files: {used_key_files}")
+    print(f"Found key files: {found_key_files}")
+
     # step 3, get all key files that are not in the ssh config
-    unused_key_files = [key_file for key_file in key_files if key_file not in used_key_files]
+    unused_key_files = [key_file for key_file in found_key_files if key_file not in used_key_files]
 
     # step 4, remove all key files that are not in the ssh config
     for key_file in unused_key_files:
 
-        if not confirm_action(f"Remove key files `{key_file}(.pub)`?"):
+        if not confirm_action(f"Remove key files `{key_file}` and `{key_file}.pub`?"):
             cprint(f"Skipping ...", "green")
             continue
 
         try:
-            os.remove(f'{KEY_DIR_PATH}/{key_file}')
+            os.remove(key_file)
             cprint(f'Removed key files `{key_file}`', "green")
         except FileNotFoundError:
             cprint(f'Key files `{key_file}` could not be removed', "yellow")
 
         try:
-            os.remove(f'{KEY_DIR_PATH}/{key_file}.pub')
+            os.remove(f'{key_file}.pub')
             cprint(f'Removed public key files `{key_file}.pub`', "green")
         except FileNotFoundError:
             cprint(f'Public key files `{key_file}.pub` could not be removed', "yellow")
